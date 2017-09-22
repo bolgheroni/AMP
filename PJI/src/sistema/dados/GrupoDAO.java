560ad058c00e732218a0b4d00e5aadfa5ddf35c6
bolgheroni <30711378+bolgheroni@users.noreply.github.com>
2017-09-21 22:02:21 -0300

    Add files via upload

---
+++ PJI/src/sistema/dados/GrupoDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.dados;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import sistema.model.Grupo;
+
+/**
+ *
+ * @author aluno
+ */
+public class GrupoDAO {
+    
+    Connection conexao;
+    
+    public void cadastrarGrupo(Grupo grupo) {
+        if (!VerificarDAO.validarCadastro(grupo)) {
+            try {
+                conexao = Conexao.abrir();
+                String sql = "INSERT INTO Grupo"; //TODO: Adicionar statement
+                PreparedStatement ps = conexao.prepareStatement(sql);
+                ps.executeUpdate();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+    
+    public boolean logarGrupo(Grupo grupo){
+        return VerificarDAO.validarPassword(grupo);
+    }
+    
+
+}
